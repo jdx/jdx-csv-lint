c006940bc9458dadb882095295159d2f64b45d5a
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-07-26 14:58:29 -0500

    initial project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{env, error::Error, ffi::OsString, fs::File, process};
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let debug = env::var("JDX_CSV_LINT_DEBUG").is_ok();
+
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record = result?;
+        // TODO: use log crate
+        if debug {
+            println!("{:?}", record);
+        }
+    }
+    println!("CSV file is valid");
+    Ok(())
+}
+
+// TODO: use clap crate
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
