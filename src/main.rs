90420e4a59f8ece01a1412ec8a07ae7369aa734c
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-07-29 13:09:48 -0500

    assert valid email addresses

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,52 @@
-use std::{env, error::Error, ffi::OsString, fs::File, process};
+use std::{env, error::Error, io, process};
+use std::fs::File;
+use std::path::PathBuf;
+use clap::Parser;
+use xx::regex;
 
-fn run() -> Result<(), Box<dyn Error>> {
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    file: PathBuf,
+}
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+fn main() {
+    let cli = Cli::parse();
+    if let Err(err) = cli.run() {
+        eprintln!("{err}");
+        process::exit(1);
+    }
+}
+
+impl Cli {
+    fn run(&self) -> Result<()> {
+        let file = File::open(&self.file)?;
+        lint(file)
+    }
+}
+
+fn lint<R: io::Read>(rdr: R) -> Result<()> {
     let debug = env::var("JDX_CSV_LINT_DEBUG").is_ok();
 
-    let file_path = get_first_arg()?;
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
+    let mut rdr = csv::Reader::from_reader(rdr);
+    let headers = rdr.headers()?;
+    let email_headers = headers.iter().enumerate().filter_map(|(i, h)| {
+        if h.eq_ignore_ascii_case("email") || h.eq_ignore_ascii_case("email_address") {
+            Some(i)
+        } else {
+            None
+        }
+    }).collect::<Vec<_>>();
     for result in rdr.records() {
         let record = result?;
+        for &i in &email_headers {
+            let email = &record[i];
+            regex!("^[^@]+@[^@]+$").find(email).ok_or_else(|| {
+                format!("Invalid email address: {}", email)
+            })?;
+        }
         // TODO: use log crate
         if debug {
             println!("{:?}", record);
@@ -17,17 +56,46 @@ fn run() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-// TODO: use clap crate
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-fn main() {
-    if let Err(err) = run() {
-        println!("{}", err);
-        process::exit(1);
+    #[test]
+    fn test_lint() {
+        let res = run(r#"a,b,c
+1,2,3
+4,5,6"#);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_lint_empty() {
+        let res = run("");
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_lint_empty_line() {
+        let res = run("\n");
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_lint_valid_email() {
+        let res = run(r#"id,email,phone
+1,foo@example.com,1234567890"#);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_lint_invalid_email() {
+        let res = run(r#"id,email,phone
+1,foo,1234567890"#);
+        assert!(res.is_err());
+    }
+
+    fn run(data: &str) -> Result<()> {
+        let rdr = io::Cursor::new(data);
+        lint(rdr)
     }
 }
